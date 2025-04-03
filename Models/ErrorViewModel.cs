94a1dc1f1731fee542047ce5ff2b8aedc88c63c2
DangThao195 <ngocthaodn0109@gmail.com>
2025-04-03 14:14:05 +0700

    Delete Models directory

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace PBL3_DLT.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}

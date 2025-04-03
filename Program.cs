c19c3cc398d07ab24138df4f0e7b5e8863afff7b
DangThao195 <ngocthaodn0109@gmail.com>
2025-04-03 14:18:13 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+using Entities;
+using InterfaceService;
+using Services;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<ApplicationDbContext> (option =>
+    {
+        option.UseSqlServer(builder.Configuration.GetConnectionString ("DefaultConnection"));
+    });
+builder.Services.AddControllersWithViews();
+var app = builder.Build();
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();

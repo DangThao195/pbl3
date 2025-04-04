eaec77ac4f1360d7d986c80295057080d4c933c6
David12405 <huudatdn1204@gmail.com>
2025-04-04 23:49:55 +0700

    Add files via upload

---
+++ Interface/IBillDetailService.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PBL3_HK4.Entity;
+namespace PBL3_HK4.Interface
+{
+    public interface IBillDetailService
+    {
+        public Task<IEnumerable<BillDetail>> GetAllBillDetailsAsync();
+        public Task<BillDetail> GetBillDetailByIdAsync(string id);
+        public Task AddBillDetailAsync(BillDetail billDetail);
+        public Task UpdateBillDetailAsync(BillDetail billDetail);
+        public Task DeleteBillDetailAsync(string id);
+        public Task<IEnumerable<BillDetail>> GetBillDetailsByBillIdAsync(string billId);
+    }
+}
